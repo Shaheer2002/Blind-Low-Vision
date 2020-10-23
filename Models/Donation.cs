4735d814ef0c1a952c8fc3ac62ef3cab056bfe23
Muhammad Shaheer Attiq <ac106722@avcol.school.nz>
2020-10-23 14:32:22 +1300

    Donation Model Created + Donation Page update (added CVV+ corrected amount)

--- Models/Donation.cs
+++ Models/Donation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,28 @@ namespace AuthSystem.Models
 {
     public class Donation
     {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Enter Name")]
+        [Display (Name = " Name :")]
+        public string PName { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Amount")]
+        [Display(Name = " Amount :")]
+        public string PAmount { get; set; }
+
+        [Required(ErrorMessage = "Enter Card Number")]
+        [Display(Name = " CardNumber :")]
+        public string PCardNumber { get; set; }
+
+        [Required(ErrorMessage = "Enter CVV")]
+        [Display(Name = " CVV :")]
+        public string PCVV { get; set; }
+
+        [Required(ErrorMessage = "Enter Expiration Date")]
+        [Display(Name = " ExpirationDate :")]
+        public string PExpirationDate { get; set; }
+        
     }
 }
