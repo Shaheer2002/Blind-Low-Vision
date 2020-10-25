83f5568c00e25e365fd31a2bd6adf3c3e35b40b3
Shaheer <shaheerattiq786@hotmail.com>
2020-10-26 12:14:11 +1300

    Fundraising challenge progress page created, Progress link added into the navbar.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace AuthSystem.Controllers
         {
             return View();
         }
+        public IActionResult Challenge()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
