c02265bba28732ae92d5d14edbb395526833b14c
Muhammad Shaheer Attiq <ac106722@avcol.school.nz>
2020-10-30 14:47:16 +1300

    2nd Test Case for Donation Table connection in Database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using AuthSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Data.SqlClient;
+using Microsoft.Data;
 
 namespace AuthSystem.Controllers
 {
@@ -49,22 +50,23 @@ namespace AuthSystem.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Donation (Donation ur)
+        public IActionResult Donation(Donation ur)
         {
             string connection = "Server=tpisql01.avcol.school.nz ; Database=MVCAuthDB-Shaheer; Trusted_Connection=True;  MultipleActiveResultSets=true";
 
 
-                using (SqlConnection sqlconn = new SqlConnection(connection))
+               using (SqlConnection sqlconn = new SqlConnection(connection))
                 {
-                 string sqlquery = "insert into DonationTable(Owner,Amount,CardNumber,ExpirationDate) values ('" + ur.Owner + "','" + ur.Amount + "','" + ur.CardNumber + "','" + ur.ExpirationDate + "')";
+                 string sqlquery = "insert into [dbo].[DonationTable](Owner,Amount,CardNumber,CVV,ExpirationDate) values ('" + ur.Owner + "','" + ur.Amount + "','" + ur.CardNumber + "','" + ur.CVV + "','"+ ur.ExpirationDate + "')";
                  using (SqlCommand sqlcomm = new SqlCommand(sqlquery,sqlconn))
-                  {
+                   {
                     sqlconn.Open();
                     sqlcomm.ExecuteNonQuery();
                     ViewData["Message"] = "The New Owner " + ur.Owner + " is saved successfully..! ";
-                  }
+                    return View(ur);
+                   }
                 }
-            return View(ur);
+            
         }
 
 
