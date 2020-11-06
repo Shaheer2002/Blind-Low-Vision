8ca0a4e6e03e8e186eb0e0e59a9ceb3d2bff9a87
Muhammad Shaheer Attiq <ac106722@avcol.school.nz>
2020-11-06 14:17:21 +1300

    Form created in challenge page (Progress)

---
+++ Models/Challenge.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Models
+{
+    public class Challenge
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Enter Name")]
+        [Display(Name = " Name :")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Fundraising Program Name")]
+        [Display(Name = " Program :")]
+        public string Program { get; set; }
+
+        [Required(ErrorMessage = "Enter Email")]
+        [Display(Name = " Email:")]
+        public string email { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Address")]
+        [Display(Name = " Address :")]
+        public string address { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Zip")]
+        [Display(Name = " Zip :")]
+        public string zip { get; set; }
+
+        [Required(ErrorMessage = "Enter City")]
+        [Display(Name = " City :")]
+        public string city { get; set; }
+
+        [Required(ErrorMessage = "Enter Country")]
+        [Display(Name = " Country :")]
+        public string country { get; set; }
+    }
+}
